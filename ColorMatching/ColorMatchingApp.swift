e5eb718894adc498bb09e58edcb4c13d06c286ea
Meng To <shadownessguy@gmail.com>
2023-03-19 06:10:04 -0400

    Initial Commit

---
+++ ColorMatching/ColorMatchingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorMatchingApp.swift
+//  ColorMatching
+//
+//  Created by Meng To on 2023-03-19.
+//
+
+import SwiftUI
+
+@main
+struct ColorMatchingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
